420ea1cee647a1a27c95876b5f81e276911830d9
begbek <Begumbekiro@gmail.com>
2022-10-25 12:33:06 +0200

    change of preprocess behavior due to memory constraint

--- preprocess.py
+++ preprocess.py
@@ -12,23 +12,30 @@ def process_images():
     
     # On regarde si les images n'ont pas déjà été processsed
     
-    if os.path.isfile("data/processed_img"):
-        print("It seems that images have already been processed : data/processed_img")
-        return
-    else:
-        os.mkdir("data/processed_img")
+    # if os.path.isfile("data/processed_img"):
+    #     print("It seems that images have already been processed : data/processed_img")
+    #     return
+    # else:
+    #     os.mkdir("data/processed_img")
+
     #Take images in /data/img_aligned_celeba, processes it and save it in /data/processed_img
     print("Redimensionnemment des images : ")
     img_tab =[]
+    count = 1
     for i in range(1, NB_IMAGES+1):
         if i%1000 == 0:
             print(i, "/", NB_IMAGES)
         img = cv.imread("data/img_align_celeba/%06i.jpg" %i)
         img = cv.resize(img, (256,256), interpolation=cv.INTER_LANCZOS4)
         img_tab.append(img)
+        if i % 20000 == 0:
+            np.savez_compressed(f"data/img_celebacomp{count}.npz", img_tab)
+            count +=1
+            img_tab = []
 
 
-    np.save("data/img_celeba.npy", img_tab)
+
+
     print("Images have been processed")
 
 
---
+++ test.py
@@ -0,0 +1,6 @@
+import numpy as np 
+
+a = np.load("data/img_celeba1.npz")['arr_0']
+print(a[0])
+
+input("ok")
