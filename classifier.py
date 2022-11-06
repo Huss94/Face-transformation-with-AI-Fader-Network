7952674f8782c2eeba41676de37bcacf5f36a27a
zakariaBOUHAFS <etudiant4510@gmail.com>
2022-11-06 23:36:42 +0100

    Ajout du parser

---
+++ classifier.py
@@ -0,0 +1,72 @@
+from model import Classifier
+import tensorflow as tf
+from tensorflow import keras
+from loader import Loader
+import numpy as np
+from utils import save_model
+from time import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Trainong of the calssifier')
+parser.add_argument("--batch_size", type = int, default = 32, help= "Size of the batch used during the training")
+parser.add_argument("--img_path", type = str, default = "data/img_align_celeba", help= "Path to images")
+parser.add_argument("--attr_path" ,type = str, default = "data/attributes.npz", help = "path to attributes")
+parser.add_argument("--attr", type = str, default= "Smiling", help= "Considered attributes to train the network with")
+parser.add_argument("--n_epoch", type = int, default = 5, help = "Numbers of epochs")
+parser.add_argument("--epoch_size", type = int, default = 50000, help = "Number of images seen at each epoch")
+
+params = parser.parse_args()
+if __name__ == '__main__':
+
+    # On reprend les paramètres utilisés par les auteurs de l'article
+    train_indices = 162770
+    val_indices = train_indices + 19867
+
+
+    Data = Loader(params, train_indices, val_indices)
+
+    C = Classifier(params)
+    C.compile(optimizer= keras.optimizers.Adam(learning_rate=0.0002))
+
+    history = {'train_loss' : [], 'train_acc': [], 'val_loss' : [], 'val_acc' : []}
+    best_acc = 0
+    # tf.config.run_functions_eagerly(True)
+
+    for epoch in range(params.n_epoch):
+        #training loop
+        loss = []
+        acc = []
+        for step in range(0, params.epoch_size, params.batch_size):
+            t = time()
+            batch_x, batch_y  = Data.load_random_batch(0, train_indices, params.batch_size)
+            l,a = C.train_step((batch_x, batch_y))
+            loss.append(l)
+            acc.append(a)
+            print(step, a.numpy(), time() - t)
+                
+        history['train_loss'].append(np.mean(loss))
+        history['train_acc'].append(np.mean(acc))
+
+        #Eval loop 
+        loss = []
+        acc = []
+        for step in range(train_indices, val_indices, params.batch_size):
+            t = time()
+            batch_x, batch_y = Data.load_batch_sequentially(step, step+params.batch_size)
+            l, a= C.eval_on_batch((batch_x, batch_y))
+            loss.append(l)
+            acc.append(a)
+            print(step, a, time() - t)
+        
+        # Peut nous permettre de tracer un graph.
+        history['val_loss'].append(np.mean(loss))
+        history['val_acc'].append(np.mean(acc))
+
+        if history['val_acc'][-1] > best_acc: 
+            best_acc = history['val_acc'][-1]
+            save_model(C, 'classifier')
+
+
+
+
+
--- loader.py
+++ loader.py
@@ -3,9 +3,10 @@ import cv2 as cv
 import tensorflow as tf
 from utils import vstack, normalize
 class Loader():
-    def __init__(self, attributes_path, images_path, considered_attributes, train_ind, val_ind):
-        self.attributes =  self.prepare_attributes(attributes_path, considered_attributes)
-        self.images_path = images_path
+    def __init__(self, params, train_ind, val_ind):
+        self.img_path = params.img_path
+        self.attr_path = params.attr_path
+        self.attributes =  self.prepare_attributes(params)
         self.train_ind = train_ind
         self.val_ind = val_ind
         
@@ -25,7 +26,7 @@ class Loader():
         batch_x, batch_y = np.array([]), np.array([])
 
         for i in indices:
-            im = cv.imread(self.images_path+"/%06i.jpg" %i)
+            im = cv.imread(self.img_path+"/%06i.jpg" %i)
             im = cv.resize(im, (256,256), interpolation=cv.INTER_LANCZOS4)
             im = normalize(im)
             im = np.expand_dims(im, 0)
@@ -35,11 +36,13 @@ class Loader():
         return tf.constant(batch_x), tf.constant(batch_y)   
 
 
-    def prepare_attributes(self, attributes_path,param_attr):
-        attributes = np.load("data/attributes.npz", allow_pickle=True)['arr_0'].item()
+    def prepare_attributes(self, params):
+        considered_attr = params.attr.replace(' ', '').split(',')
+        params.n_attr = len(considered_attr)
+        attributes = np.load(self.attr_path, allow_pickle=True)['arr_0'].item()
         attr = None
-        for p in param_attr:
-            for i in range(2):
+        for p in considered_attr:
+            for i in range(params.n_attr):
                 formated_attr = np.reshape(attributes[p] == i, (len(attributes[p]), 1))
                 if attr is None:
                     attr = np.array(formated_attr.astype(np.float32))
